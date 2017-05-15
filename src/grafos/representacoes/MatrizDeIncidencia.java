4ab44e81ee13392e1f6adaf9d5518bd2b5a1398a
Beatriz Rodrigues <beatrizrodrigues19.br@gmail.com>
2017-05-15 11:02:51 -0300

    grafos2017

--- src/grafos/representacoes/MatrizDeIncidencia.java
+++ src/grafos/representacoes/MatrizDeIncidencia.java
@@ -5,10 +5,64 @@
  */
 package grafos.representacoes;
 
+import java.util.List;
+
 /**
  *
  * @author Acer
- */
-public class MatrizDeIncidencia {
+ */ 
+public class MatrizDeIncidencia implements Grafo{
+    
+    private int[][] matrizIncidencia;
+
+    public MatrizDeIncidencia(int numVertices) {
+        
+        this.matrizIncidencia = new int[numVertices][(numVertices*(numVertices -1))/2];
+    }
+ 
+    @Override
+    public int getNumeroDeVertices() {
+        return 0;
+    }
+
+    @Override
+    public void addAresta(int origem, int destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addAresta(int origem, int destino, double peso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAresta(int origem, int destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAresta(int origem, int destino, double peso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeAresta(int origem, int destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isAdjacente(int origem, int destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getAdjacentes(int vertice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getPeso(int origem, int destino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
